cdfaf316695091548d792566391ea56d300187b0
Jeesoo <jeesooyoon@kaist.ac.k>
2019-06-29 17:45:25 +0900

    added encrypt function

--- app/src/main/java/com/example/firstapp/ui/main/ImageEncryption.java
+++ app/src/main/java/com/example/firstapp/ui/main/ImageEncryption.java
@@ -2,16 +2,21 @@ package com.example.firstapp.ui.main;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.Image;
 
+import static android.graphics.Bitmap.Config.ARGB_8888;
+import static android.graphics.Bitmap.createBitmap;
+
 public class ImageEncryption {
-    public Bitmap Encrypt(String backGround, String secret) {
-        Bitmap backGroundImage = BitmapFactory.decodeFile(backGround);
-        Bitmap secretImage = BitmapFactory.decodeFile(secret);
-        Bitmap encryptImage = BitmapFactory.decodeFile(backGround);
+    //public Bitmap Encrypt(String backGround, String secret) {
+    public Bitmap Encrypt (Bitmap backGroundImage, Bitmap secretImage) {
+        //Bitmap backGroundImage = BitmapFactory.decodeFile(backGround);
+        //Bitmap secretImage = BitmapFactory.decodeFile(secret);
+        Bitmap encryptImage = createBitmap(backGroundImage.getWidth(), backGroundImage.getHeight(), ARGB_8888);//backGroundImage ;//Bitmap.createBitmap(backGroundImage); //BitmapFactory.decodeFile(backGround);
         int p, a, b, c, d, e, f;
 
-        if (backGroundImage.getHeight() - secretImage.getHeight() != 0 || backGroundImage.getWidth() - secretImage.getWidth() != 0)
+        if (backGroundImage.getHeight() != secretImage.getHeight() || backGroundImage.getWidth() != secretImage.getWidth())
         {
             return backGroundImage;
         }
@@ -28,16 +33,22 @@ public class ImageEncryption {
                 a = (16 * (a / 16) + d / 16) << 16;
                 b = (16 * (b / 16) + e / 16) << 8;
                 c = 16 * (c / 16) + f / 16;
-                encryptImage.setPixel(j, i, a + b + c);
+
+                try {
+                    encryptImage.setPixel(j, i, a+b+c);//0x008577);
+                } catch (IllegalStateException z){
+                    z.printStackTrace();
+                }
             }
         }
         return encryptImage;
     }
 
-    public Bitmap Decrypt (String encrypt) {
-        Bitmap encryptImage = BitmapFactory.decodeFile(encrypt);
-        Bitmap decryptImage = BitmapFactory.decodeFile(encrypt);
+    public Bitmap Decrypt (Bitmap encryptImage) {
+        Bitmap decryptImage = encryptImage;// createBitmap(encryptImage.getWidth(), encryptImage.getHeight(), ARGB_8888);
         int p, a, b, c;
+
+
 
         for (int i = 0; i < encryptImage.getHeight(); i++)
         {
@@ -50,7 +61,7 @@ public class ImageEncryption {
                 a = (16 * (a % 16)) << 16;
                 b = (16 * (b % 16)) << 8;
                 c = 16 * (c % 16);
-                decryptImage.setPixel(j,i, a + b + c);
+//                decryptImage.setPixel(j,i, a + b + c);
             }
         }
         return decryptImage;
